eda6f7ff5d2dba4d11546b1afe98768283e3c791
absiddique10 <mdabubokorsiddique10@gmail.com>
2024-03-03 23:02:36 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homework Php</title>
+</head>
+<body>
+<?php   
+    $number=1;
+    while ($number< 50) {
+        echo $number ."</br>";
+        $number=$number+2;
+    }
+
+
+    $squnum=1;
+        while ($squnum< 10) {
+            echo $squnum*$squnum ."<br>";
+            $squnum++;
+        }
+    ?>
+
+
+</body>
+</html>
